3a5c887683d92766be3ff0d1e1ed1c7a65efb00e
esadakcam <m.esadakcam@hotmail.com>
2021-11-22 15:12:08 +0300

    add author, show all authors, flash messages for database errors are added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -16,8 +17,39 @@ def showbooks():
 
 @app.route("/author/showauthor")
 def showauthor():
-    return render_template("show_authors.html")
+
+    con = sqlite3.connect("Bookshop.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Author")
+    rows = cur.fetchall()
+    return render_template("./author/show_authors.html", rows=rows)
+
+
+@app.route("/author/addauthor", methods=["POST", "GET"])
+def addauthor():
+    if request.method == "POST":
+        try:
+            authorId = request.form["authorId"]
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+            birthday = request.form["birthday"]
+            country = request.form["country"]
+            hrs = request.form["hrs"]
+            with sqlite3.connect("Bookshop.db") as con:
+                cur = con.cursor()
+                cur.execute(
+                    f'Insert Into AUTHOR Values("{authorId}","{firstName}","{lastName}","{birthday}","{country}", "{hrs}")')
+
+                flash("successful")
+        except:
+            con.rollback()
+            # flash("failed")
+        finally:
+            con.close()
+    return render_template("./author/addauthor.html")
 
 
 if __name__ == "__main__":
+    app.secret_key = 'the random string'
     app.run(debug=True)
